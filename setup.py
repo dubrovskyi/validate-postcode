d4b094dfe7185f873a2a5f65beaebec1e66ec9de
vova <martiniden0@gmail.com>
2025-06-18 14:50:00 +0300

    add postcode validation library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='postcode-validator-',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='Tool for validation postcodes',
+    author='dubrovskyi',
+    license='MIT',
+)
